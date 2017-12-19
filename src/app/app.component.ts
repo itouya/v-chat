f613fe1e548a263b08a8e1fb4e0791839f2669e5
Yasuhiko ITOH <itouya@alpha.co.jp>
2017-12-19 18:18:43 +0900

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  peer: any;
+  localStream: any;
+
+  ngOnInit() {
+    navigator.mediaDevices.getUserMedia({audio: true, video: true})
+      .then(stream => {
+        let element: any;
+        element = $('#my-video').get(0) as HTMLMediaElement;
+        element.srcObject = stream;
+      });
+    this.peer = new Peer({key: 'af210551-c65c-4a73-bc1e-0ae11a02b32e', debug: 3});
+    this.peer.on('open', (id) => {
+      console.log('peer.id: ' + id);
+    });
+  }
 }
